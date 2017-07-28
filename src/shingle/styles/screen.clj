0e0b5c35cd7f75ddd41bb1480387282f296ffe11
Matthew M. Nelson <dogenpunk@gmail.com>
2017-07-28 14:13:07 -0500

    Add contact form and additional background images
    Images from https://unsplash.com/
    
    Photo credits: Jakob Owens, Ludde Lorentz, and Eduard Militaru

--- src/shingle/styles/screen.clj
+++ src/shingle/styles/screen.clj
@@ -1,8 +1,25 @@
 (ns shingle.styles.screen
   (:require [garden.units :as gu :refer [px em vh vw rem percent]]
-            [garden.def :as gd :refer [defstylesheet defstyles]]
             [garden.color :as color :refer [rgba]]
             [garden.stylesheet :refer [at-import at-media]]))
+
+(defmacro defbreakpoint [name media-params]
+  `(defn ~name [& rules#]
+     (at-media ~media-params
+               [:& rules#])))
+
+(def portrait-orientation
+  {:screen true
+   :orientation :portrait})
+
+(def small-screen
+  {:screen true
+   :max-width (px 720)})
+
+(def html
+  [:html {:box-sizing :border-box
+          :cursor :default
+          :line-height 1.5}])
 
 (def base
   [:body {:background "#222"
@@ -52,25 +69,60 @@
    [:h2 {:margin 0
          :text-transform :uppercase
          :font-size (px 38)
-         :letter-spacing (px 3.8)
+         :letter-spacing (px 2.8)
          :transition "letter-spacing 0.5s"}]
    [:h3 {:margin 0}]])
+
+(def contact-form
+  [:#contact {:height "auto !important"}
+   [:form {:max-width (percent 60)
+           :padding "100px 20px"}
+    [:input
+     :textarea
+     :select {:width (percent 100)
+              :color "#fff"
+              :font :inherit
+              :background :none
+              :outline :none
+              :border 0
+              :border-radius :none
+              :border-bottom "1px solid rgba(255, 255, 255, 0.3)"
+              :padding "20px 0 10px 0"
+              :transition "border-bottom-color 0.5s"
+              :-webkit-appearance :none
+              :-moz-appearance :none}]
+    [:textarea {:height (px 150)
+                :resize :none}]
+    [:button {:cursor :pointer
+              :background "#00c8c8"
+              :border 0
+              :padding 0
+              :margin-top (px 10)
+              :height (px 60)
+              :font-size (px 21)
+              :font-weight :bold
+              :width (percent 100)
+              :letter-spacing (px 3)}]]])
 
 (def footer
   [:footer {:display :flex}
    [:h3 {:flex 1
          :margin 0
-         :font-size (px 24)
+         :font-size (em 1.333)
          :letter-spacing (px 1)
          :line-height (px 100)
          :text-align :center}]
    [:a {:display :block}]])
 
 (def styles
-  [base
+  [html
+   base
    main
    links
    logo
    sections
    content
-   footer])
+   contact-form
+   footer
+   (at-media small-screen
+          [:footer {:display :block}])])
